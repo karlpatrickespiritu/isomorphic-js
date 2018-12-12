02ddddb0d8234598c47affe092f12f656dfe3513
karlpatrickespiritu <wiwa.espiritu@gmail.com>
2018-12-12 10:21:49 +0800

    udpates

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,30 +1,7 @@
-import requestPromise from 'request-promise';
 import coinsMock from '../shared/mock/coins';
-
-const API_KEY = 'a0872a16-370d-4eba-bd33-206fa57992bf'; // live
 
 export function fetchCoins() {
   return new Promise((resolve, reject) => {
-    const requestOptions = {
-      method: 'GET',
-      uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-      qs: {
-        start: 1,
-        limit: 50,
-        convert: 'USD'
-      },
-      headers: {
-        'X-CMC_PRO_API_KEY': API_KEY
-      },
-      json: true,
-      gzip: true
-    };
-
     resolve(coinsMock);
-    // requestPromise(requestOptions).then(response => {
-    //   resolve(response.data);
-    // }).catch((err) => {
-    //   reject(err);
-    // });
   });
 }
