5f5e8ae0f8b1fc5558a9748cca970984cdb97a1c
karlpatrickespiritu <wiwa.espiritu@gmail.com>
2018-12-12 10:19:49 +0800

    udpates

--- src/server/index.js
+++ src/server/index.js
@@ -47,10 +47,3 @@ app.get("*", (req, res, next) => {
 app.listen(3000, () => {
   console.log(`Server is listening on port: 3000`)
 });
-
-/*
-  1) Just get shared App rendering to string on server then taking over on client.
-  2) Pass data to <App /> on server. Show diff. Add data to window then pick it up on the client too.
-  3) Instead of static data move to dynamic data (github gists)
-  4) add in routing.
-*/
